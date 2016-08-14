601c5cc45eb99bc199577cc9dce085ae9d5ab979
FunnyWu <agiot1026@163.com>
2016-08-14 19:02:12 +0800

    第一版

--- src/com/srun3000/View/aboutMenu.java
+++ src/com/srun3000/View/aboutMenu.java
@@ -9,7 +9,7 @@ import java.awt.*;
 public class aboutMenu extends JTextArea {
     public aboutMenu() {
         setFont(new Font("宋体", Font.BOLD, 40));
-        setText("小白菜：\n工科男一枚，热爱技术，喜欢专研。\n英语很渣，很渣，很渣。\n感谢：此作品受AnotherKnife的作品启发开发而来\n微信号(FunnyWu1026)欢迎骚扰。");
+        setText("小白菜：\n工科男一枚，热爱技术，喜欢专研。\n英语很渣，很渣，很渣。\n感谢：此作品受AnotherKnife(计科15)的作品启发开发而来\n微信号(FunnyWu1026)欢迎骚扰。");
         setLineWrap(true);
         setEditable(false);
         setWrapStyleWord(true);
